62e9f0f7f0c3bb67e246ced5869f18550a113ed5
Fabian Quesada <fabian.quesada.s@gmail.com>
2012-12-12 14:26:24 -0600

    Prueba de uso de Git con NetBeans

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,7 +4,7 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<h1>Hello<i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
 <p>Congratulations! You have successfully created your Yii application.</p>
 
