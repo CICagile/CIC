085aff6c6c692370f0121cb43ed94c98af5e0cdf
Mario Retana <maretana@ic-itcr.ac.cr>
2012-12-18 17:08:17 -0600

    Cambié el título del sitio a Página CIC.

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Página CIC',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
