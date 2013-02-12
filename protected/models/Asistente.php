e01e84e210c31e9701c9786231c476393fb042f6
Mario Retana <maretana@ic-itcr.ac.cr>
2013-02-12 09:36:26 -0600

    Se cambió el mensaje de error de las horas cuando estan exceden el máximo.

--- protected/models/Asistente.php
+++ protected/models/Asistente.php
@@ -43,7 +43,7 @@ class Asistente  extends CModel{
                 array('banco', 'length', 'max'=>70),
                 array('cuentacliente', 'length', 'min'=>17, 'max'=>17),
                 array('telefono, cedula, cuentacliente, carnet', 'match', 'pattern'=>'/^[\p{N}]+$/u', 'message'=>'{attribute} sólo puede estar compuesto por dígitos.'),
-                array('horas', 'numerical', 'max'=>20, 'min'=>1, 'tooBig'=>'Se permite un máximo de {max} horas', 'tooSmall'=>'Se permite un mínimo de {min} horas.'),
+                array('horas', 'numerical', 'max'=>20, 'min'=>1, 'tooBig'=>'Se permite un máximo de {max} horas de asistencia', 'tooSmall'=>'Se permite un mínimo de {min} horas.'),
                 array('correo', 'email', 'message'=>'Dirección de correo inválida'),
                 array('nombre, apellido1, apellido2, ', 'match', 'pattern'=>'/^[\p{L} ]+$/u'),
                 array('numerocuenta,codigo', 'match', 'pattern'=>'/^[\p{N}-]+$/u'),
