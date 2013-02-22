3c8d82443715691e5b559ad3afc7847feffe75b9
Fabian Quesada <fabian.quesada.s@gmail.com>
2013-02-22 11:40:38 -0600

    Crud de Tipo de Proyecto, controlador, vista admin, create y update y validacion Modelo

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -27,15 +27,14 @@
 	</div><!-- header -->
 
 	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
+		<?php $this->widget('zii.widgets.CMenu',array(                        
 			'items'=>array(
 				array('label'=>'Inicio', 'url'=>array('/site/index')),
 				//array('label'=>'Acerca de...', 'url'=>array('/site/page', 'view'=>'about')),
 				//array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
-                            )
-                    ,
+				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),                                                         
+                        ),                           
 		)); ?>
 	</div><!-- mainmenu -->
 	<?php if(isset($this->breadcrumbs)):?>
