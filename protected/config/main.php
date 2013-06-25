0a5e450d75892cf1d1d9d038b52d9acfc1736055
Mario Retana <maretana@ic-itcr.ac.cr>
2013-06-25 10:28:46 -0600

    Merge con los branch cancelar proyecto y asistente por sector beneficiado

--- protected/config/main.php
+++ protected/config/main.php
@@ -52,22 +52,22 @@ return array(
 		),*/
 		// uncomment the following to use a MySQL database
 		
-		'db'=>array(
+		/*'db'=>array(
                        // 'connectionString' => 'mysql:host=169.254.150.189;dbname=db_cic',
 			'connectionString' => 'mysql:host=172.19.16.219;dbname=db_cic',
 			'emulatePrepare' => true,
 			'username' => 'CIC',
 			'password' => 'Cicagile004',
 			'charset' => 'utf8',
-		),
+		),*/
             
-//                'db'=>array(
-//			'connectionString' => 'mysql:host=localhost;dbname=db_cic',
-//			'emulatePrepare' => true,
-//			'username' => 'root',
-//			'password' => 'CICagile004',
-//			'charset' => 'utf8',
-//		),
+                'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=db_cic',
+			'emulatePrepare' => true,
+			'username' => 'root',
+			'password' => '',
+			'charset' => 'utf8',
+		),
 		
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
