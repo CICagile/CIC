2c4b689bd43e570d84c2541878848d4c38b3557a
Mario Retana <maretana@ic-itcr.ac.cr>
2013-07-18 20:32:03 -0600

    Cambio en la funcion de buscar datos actuales del asistente en un proyecto

--- protected/models/Asistente.php
+++ protected/models/Asistente.php
@@ -177,16 +177,15 @@ class Asistente  extends CModel{
     /**
      * Busca los datos más actuales de un asistente en un proyecto.
      * Carga los datos en el modelo y guarda los periódos en un array.
-     * @param string $pCarnet Carnet del estudiante buscado.
      * @param int $pIDProyecto ID del proyecto en la base de datos.
      * @return array Un arreglo de arreglos con la fechas de los periodos correspondientes a los
      * atributos que cargó de la base de datos.
      */
-    public function buscarDatosActualesAsistenteEnProyecto($pCarnet, $pIDProyecto) {
+    public function buscarDatosActualesAsistenteEnProyecto($pIDProyecto) {
         $respuesta =  NULL;
         $call = 'CALL buscarDatosActualesAsistenteEnProyecto(:carnet,:idproyecto)';
         $comando = Yii::app()->db->createCommand($call);
-        $comando->bindParam(':carnet', $pCarnet);
+        $comando->bindParam(':carnet', $this->carnet);
         $comando->bindParam(':idproyecto', $pIDProyecto);
         $query = $comando->query();
         if ($query->rowCount === 1) {
