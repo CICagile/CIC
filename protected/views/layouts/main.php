82f23cfe26f7d522da84891864cb559898e7acd1
Jose Pablo Garbanzo <jpgarbanzo@gmail.com>
2013-08-22 15:41:02 -0600

    Se actualiza número de versión

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -52,7 +52,7 @@
 	<div id="footer">
 		&copy; Centro de Investigación en Computación del ITCR <?php echo date('Y'); ?><br/>
 		Todos los derechos reservados.<br/>
-                Version 0.4 <br/>
+                Version 0.5 <br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
 
