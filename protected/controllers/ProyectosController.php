2fb7e0d7dda75c8190abeddc224adb23665d39f9
Mario Retana <maretana@ic-itcr.ac.cr>
2013-09-09 01:32:18 -0600

    Se cambia el nombre de una función para que cumpla con los estándares de programación del proyecto

--- protected/controllers/ProyectosController.php
+++ protected/controllers/ProyectosController.php
@@ -55,7 +55,7 @@ class ProyectosController extends Controller {
             $this->render('ver', array(
                 'model' => $model,
                 'dataProvider' => $model->buscarAsistentesActivosDeProyecto(),
-                'investigadores' => $model->buscarinvestigadorporproyecto(),
+                'investigadores' => $model->buscarInvestigadoresActivos(),
             ));
     }
 
