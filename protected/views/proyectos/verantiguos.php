0495191befd0d6fc0d528bd85463ccd8596cbe8b
Mario Retana <maretana@ic-itcr.ac.cr>
2013-09-10 19:04:22 -0600

    no es necesario mostrar los asistentes activos de un proyecto antiguo.

--- protected/views/proyectos/verantiguos.php
+++ protected/views/proyectos/verantiguos.php
@@ -1,8 +1,6 @@
 <?php
 /* @var $this ProyectosController */
 /* @var $model Proyectos */
-/* @var $asistente Asistente */
-/* @var $dataProvider CArrayDataProvider */
 $this->breadcrumbs = array(
     'Proyectos' => array('admin'),
     $model->codigo,
@@ -13,45 +11,6 @@ $this->menu = array(
     array('label' => 'Ver proyectos activos', 'url' => array('admin')),
     array('label' => 'Ver proyectos antiguos', 'url' => array('adminantiguos')),
 );
-
-//Columnas de la tabla de los asistentes activos del proyecto.
-$columns = array(
-    array(
-        'header' => CHtml::encode($asistente->getAttributeLabel('carnet')),
-        'name' => 'carnet',
-    ),
-    array(
-        'header' => CHtml::encode($asistente->getAttributeLabel('nombre')),
-        'name' => 'nombre',
-    ),
-    array(
-        'header' => CHtml::encode($asistente->getAttributeLabel('apellido1')),
-        'name' => 'apellido1',
-    ),
-    array(
-        'header' => CHtml::encode($asistente->getAttributeLabel('apellido2')),
-        'name' => 'apellido2',
-    ),
-    array(
-        'header' => CHtml::encode($asistente->getAttributeLabel('rol')),
-        'name' => 'rol',
-    ),
-    array(
-        'header' => CHtml::encode('Horas'),
-        'name' => 'horas',
-    ),
-    array(
-        'header' => CHtml::encode('Fin de la asistencia'),
-        'name' => 'fin',
-    ),
-    array(
-        'class' => 'CButtonColumn',
-        'template' => '{view}{update}',
-        'viewButtonUrl' => 'Yii::app()->controller->createUrl("Asistente/view", array("id"=>$data["carnet"]))',
-        'viewButtonLabel' => 'Ver información detallada del asistente',
-        'updateButtonUrl' => 'Yii::app()->controller->createUrl("Asistente/updateDP", array("id"=>$data["carnet"]))',
-        'updateButtonLabel' => 'Editar información personal del asistente',
-        ));
 ?>
 
 <h3>Detalle del proyecto.</h3>
@@ -81,18 +40,5 @@ $this->widget('zii.widgets.CDetailView', array(
             'type' => 'html',
         ),
     ),
-));
-?>
-
-<br/>
-<br/>
-<h3>Asistentes</h3>
-
-<?php
-$this->widget('zii.widgets.grid.CGridView', array(
-    'id' => 'asistente-grid',
-    'dataProvider' => $dataProvider,
-    //'filter'=>$model,
-    'columns' => $columns,
 ));
 ?>
