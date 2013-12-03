aa9c6d396e8d2f0b101b511bd822e4de0837a73b
agil1 <agil1@CIC-002.ic-itcr.ac.cr>
2013-12-03 11:45:27 -0600

    Reporte de horas de investigadores terminado

--- protected/views/investigador/_reporteHorasMes.php
+++ protected/views/investigador/_reporteHorasMes.php
@@ -5,7 +5,7 @@ if ($data_provider != null) {
         'id' => 'investigador-grid-historial',
         'dataProvider' => $data_provider,
         'columns' => array(
-            array('header' => 'Código del Proyecto', 'value' => '$data["codigo"]') /*'CHtml::link($data["codigo"], CHtml::normalizeUrl(array("/proyectos/","ver" => $data["idtbl_Proyectos"])))', 'type' => 'raw')*/,
+            array('header' => 'Código del Proyecto', 'value' => /*'$data["codigo"]') */'CHtml::link($data["codigo"], CHtml::normalizeUrl(array("/proyectos/","ver" => $data["idtbl_proyectos"])))', 'type' => 'raw'),
             array('header' => 'Horas', 'value' => '$data["horas"]'),
             array('header' => 'Inicio', 'value' => '$data["inicio"]'),
             array('header' => 'Fin', 'value' => '$data["fin"]'),
