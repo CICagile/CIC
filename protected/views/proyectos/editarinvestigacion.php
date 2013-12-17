57e015c45c9a3aa0cd5137e4cffae30d9a5e2720
Mario Retana <maretana@ic-itcr.ac.cr>
2013-12-17 10:31:53 -0600

    Cambios para el formulario de cambio en la investigación.

--- protected/views/proyectos/editarinvestigacion.php
+++ protected/views/proyectos/editarinvestigacion.php
@@ -2,11 +2,11 @@
 /* @var $this ProyectosController */
 /* @var $model Proyectos */
 /* @var $periodos array */
-/* @var $asistente Asistente */
+/* @var $investigador Investigador */
 $this->breadcrumbs=array(
 	'Proyectos'=>array('admin'),
 	$model->codigo => array('ver','id'=>$model->idtbl_Proyectos),
-        'Editar asistencia',
+        'Editar Investigación',
 );
 
 $this->menu=array(	
@@ -19,22 +19,22 @@ $this->menu=array(
 
 ?>
 
-<h1>Editar asistencia de <?php echo $asistente->carnet?></h1>
+<h1>Editar Investigación de <?php echo $investigador->nombre . ' ' . $investigador->apellido1;?></h1>
 <div class="form">
     <p class="note">Los campos marcados con <span class="required">*</span> son obligatorios.</p>
-    <?php $form = $this->beginWidget('CActiveForm', array(
+    <?php /*$form = $this->beginWidget('CActiveForm', array(
         'id' => 'editar-rol',
     )); ?>
     <fieldset>
         <legend><h2>Cambiar Rol</h2></legend>
-        <?php   $errorHoras = $asistente->getErrors(); //Se guardan por si el formulario de horas los necesita.
-                $asistente->clearErrors('horas'); //para que no muestre los errores del formulario de horas.?> 
-        <?php echo $form->errorSummary(array($periodos['rol'],$asistente),'Se han detectado los siguientes errores:'); ?>
+        <?php /*$errorHoras = $investigador->getErrors(); //Se guardan por si el formulario de horas los necesita.
+                $investigador->clearErrors('horas'); //para que no muestre los errores del formulario de horas.?> 
+        <?php echo $form->errorSummary(array($periodos['rol'],$investigador),'Se han detectado los siguientes errores:'); ?>
         <div class="row">
-            <?php echo $form->labelEx($asistente,'rol'); ?>
-            <?php echo $form->dropDownList($asistente, 'rol',
+            <?php echo $form->labelEx($investigador,'rol'); ?>
+            <?php echo $form->dropDownList($investigador, 'rol',
                     CHtml::listData(RolAsistente::model()->findAll(), 'idtbl_RolesAsistentes', 'nombre'), array('empty'=>'Elija un rol')); ?>
-            <?php echo $form->error($asistente,'rol'); ?>
+            <?php echo $form->error($investigador,'rol'); ?>
         </div>
         <div class="row">
             <?php echo $form->labelEx($periodos['rol'], 'inicio'); ?>
@@ -73,13 +73,13 @@ $this->menu=array(
     )); ?>
     <fieldset>
         <legend><h2>Cambiar Horas</h2></legend>
-        <?php   $asistente->addErrors($errorHoras); //Se restauran los errores que fueron borrados.
-                $asistente->clearErrors('rol'); //para que no muestre los errores del formulario de roles.?> 
-        <?php echo $form->errorSummary(array($periodos['horas'],$asistente),'Se han detectado los siguientes errores:'); ?>
+        <?php   $investigador->addErrors($errorHoras); //Se restauran los errores que fueron borrados.
+                $investigador->clearErrors('rol'); //para que no muestre los errores del formulario de roles.?> 
+        <?php echo $form->errorSummary(array($periodos['horas'],$investigador),'Se han detectado los siguientes errores:'); ?>
         <div class="row">
-            <?php echo $form->labelEx($asistente,'horas'); ?>
-            <?php echo $form->textField($asistente,'horas',array('size'=>4,'maxlength'=>4)); ?>
-            <?php echo $form->error($asistente,'horas'); ?>
+            <?php echo $form->labelEx($investigador,'horas'); ?>
+            <?php echo $form->textField($investigador,'horas',array('size'=>4,'maxlength'=>4)); ?>
+            <?php echo $form->error($investigador,'horas'); ?>
         </div>
         <div class="row">
             <?php echo $form->labelEx($periodos['horas'], 'inicio'); ?>
@@ -111,19 +111,19 @@ $this->menu=array(
         </div>
     </fieldset>
     <?php $this->endWidget() ?>
-</div>
+</div>*/?>
 <div class="form">
     <?php $form = $this->beginWidget('CActiveForm', array(
         'id' => 'editar-horas',
     )); ?>
     <fieldset>
-        <legend><h2>Cambiar Período de Asistencia</h2></legend>
-        <?php echo $form->errorSummary($periodos['asistencia'],'Se han detectado los siguientes errores:'); ?>
+        <legend><h2>Cambiar Período de Investigación</h2></legend>
+        <?php echo $form->errorSummary($periodos['investigacion'],'Se han detectado los siguientes errores:'); ?>
         <div class="row">
-            <?php echo $form->labelEx($periodos['asistencia'], 'inicio'); ?>
+            <?php echo $form->labelEx($periodos['investigacion'], 'inicio'); ?>
             <?php $this->widget('zii.widgets.jui.CJuiDatePicker', array(
-                'name' => 'Asistencia[inicio]',
-                'value' => $periodos['asistencia']->attributes['inicio'],
+                'name' => 'Investigacion[inicio]',
+                'value' => $periodos['investigacion']->attributes['inicio'],
                 'language' => 'es',
                 'options' => array(
                     'showAnim' => 'fold',
@@ -136,13 +136,13 @@ $this->menu=array(
                     'readonly' => 'readonly'
                 ),
             )); ?>
-            <?php echo $form->error($periodos['asistencia'],'inicio'); ?>
+            <?php echo $form->error($periodos['investigacion'],'inicio'); ?>
         </div>
         <div class="row">
-            <?php echo $form->labelEx($periodos['asistencia'], 'fin'); ?>
+            <?php echo $form->labelEx($periodos['investigacion'], 'fin'); ?>
             <?php $this->widget('zii.widgets.jui.CJuiDatePicker', array(
-                'name' => 'Asistencia[fin]',
-                'value' => $periodos['asistencia']->attributes['fin'],
+                'name' => 'Investigacion[fin]',
+                'value' => $periodos['investigacion']->attributes['fin'],
                 'language' => 'es',
                 'options' => array(
                     'showAnim' => 'fold',
@@ -155,7 +155,7 @@ $this->menu=array(
                     'readonly' => 'readonly'
                 ),
             )); ?>
-            <?php echo $form->error($periodos['asistencia'],'fin'); ?>
+            <?php echo $form->error($periodos['investigacion'],'fin'); ?>
             <div class="row-buttons">
                 <?php echo CHtml::submitButton("Guardar período"); ?>
             </div>
