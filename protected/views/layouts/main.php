fb392dd7982ecbc5e59cbfc953768a8bc74ce1db
Mario Retana <maretana@ic-itcr.ac.cr>
2013-12-19 14:40:21 -0600

    Se arregla un problema en la BD con la forma en que se guradan los Investigadores. Se eliminan dos tablas que sobran y se corrigen los stored procedures correspondientes.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -52,7 +52,7 @@
 	<div id="footer">
 		&copy; Centro de Investigación en Computación del ITCR <?php echo date('Y'); ?><br/>
 		Todos los derechos reservados.<br/>
-                Version 0.6 <br/>
+                Version 0.6.1 <br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
 
